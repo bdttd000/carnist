573be7e590ea4e7d23877c400f63195b3cb858fa
Jakub Wartak <kuba428@op.pl>
2023-06-07 04:52:46 +0200

    routing and database setup

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+const USERNAME = 'root';
+const PASSWORD = 'root';
+const HOST = '172.19.0.4';
+const DATABASE = 'postgres';
+const PORT = '5432';
---
+++ public/views/home.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'Database.php';
+
+$database = new Database();
+
+$database->connect();
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="icon" href="public/img/logo.svg" type="image/svg+xml">
+    <title>Główna</title>
+</head>
+
+<body>
+    <main>home</main>
+</body>
+
+</html>
