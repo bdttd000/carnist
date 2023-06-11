4dbdee7439272f003b64b45ea26dea95e9902d22
Jakub Wartak <kuba428@op.pl>
2023-06-11 22:44:43 +0200

    pagin page and styling setup

---
+++ public/views/login.php
@@ -0,0 +1,20 @@
+<html lang="en">
+
+<head>
+    <?php include('public/views/components/headImports.php'); ?>
+    <title>Logowanie</title>
+</head>
+
+<body>
+    <main class="login container">
+        <div class="login-logo">
+            <img class="login-logo-img" src="public/img/logo.svg" alt="asdasd">
+            <h1 class="login-logo-text">CARNIST</h1>
+        </div>
+        <form class="login-form">
+            asddas
+        </form>
+    </main>
+</body>
+
+</html>
