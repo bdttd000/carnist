7d477ad171873cdbb03c6c4fbce4b06d6390581c
Jakub Wartak <kuba428@op.pl>
2023-06-30 21:09:57 +0200

    home fix

--- public/views/home.php
+++ public/views/home.php
@@ -26,25 +26,26 @@ if ($SessionController::isLogged() === false) {
     <main>
         <div class="container">
             <section class="card_container">
-                <?php foreach ($cars as $car): ?>
-                    <a href="car?id=<?= $car->getCarId(); ?>" class="card">
-                        <div class="card_info">
-                            <h2>
-                                <?= $car->getCarInfo()->getName(); ?>
-                            </h2>
-                            <p>
-                                <?php
-                                echo strlen($car->getCarInfo()->getDescription()) > 200 ? substr($car->getCarInfo()->getDescription(), 0, 300) . '...' : $car->getCarInfo()->getDescription();
-                                ?>
-                            </p>
-                        </div>
-                        <div class="card_img_container">
-                            <img class="card_img"
-                                src="public/uploads/<?= $car->getCarInfo()->getDirectoryUrl(); ?>/<?= $car->getCarInfo()->getAvatarUrl(); ?>"
-                                alt="Zdjecie auta">
-                        </div>
-                    </a>
-                <?php endforeach; ?>
+                <?php if ($cars)
+                    foreach ($cars as $car): ?>
+                        <a href="car?id=<?= $car->getCarId(); ?>" class="card">
+                            <div class="card_info">
+                                <h2>
+                                    <?= $car->getCarInfo()->getName(); ?>
+                                </h2>
+                                <p>
+                                    <?php
+                                    echo strlen($car->getCarInfo()->getDescription()) > 200 ? substr($car->getCarInfo()->getDescription(), 0, 300) . '...' : $car->getCarInfo()->getDescription();
+                                    ?>
+                                </p>
+                            </div>
+                            <div class="card_img_container">
+                                <img class="card_img"
+                                    src="public/uploads/<?= $car->getCarInfo()->getDirectoryUrl(); ?>/<?= $car->getCarInfo()->getAvatarUrl(); ?>"
+                                    alt="Zdjecie auta">
+                            </div>
+                        </a>
+                    <?php endforeach; ?>
             </section>
         </div>
     </main>
